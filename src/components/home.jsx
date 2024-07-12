8762f9d1c8843b2bf89c70fea0842ec4ea7d83b0
PaingSuSan <149450937+PaingSuSan@users.noreply.github.com>
2024-07-12 16:09:55 +0800

    Add Home Page Buttons

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,56 +1,46 @@
 import React from "react";
 
 export const Home = () => {
-  return (
-    <section id="colorlib-hero" className="js-fullheight" data-section="home">
-      <div className="flexslider js-fullheight">
-        <ul className="slides">
-          {/* <li style={{ backgroundImage: "url(images/ITGirl.jpg)" }}> */}
-          <li>
-            <div className="overlay"></div>
-            <div className="container-fluid">
-              <div className="row">
-                <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
-                  <div className="slider-text-inner js-fullheight">
-                    <div className="desc">
-                      <h1>
-                        Hi! <br />I am Paing Su San
-                      </h1>
+  const handleDownloadPDF = () => {
+    const link = document.createElement("a");
+    link.href = `${process.env.PUBLIC_URL}/PaingSuSan_Resume.pdf`; // Path to the PDF resume file
+    link.download = "PaingSuSan_Resume.pdf"; // Desired file name
+    link.click();
+  };
 
-                      <p>
-                        <a className="btn btn-primary btn-learn">
-                          Download CV <i className="icon-download4"></i>
-                        </a>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
+  const handleDownloadWord = () => {
+    const link = document.createElement("a");
+    link.href = `${process.env.PUBLIC_URL}/PaingSuSan_Resume.docx`; // Path to the Word resume file
+    link.download = "PaingSuSan_Resume.docx"; // Desired file name
+    link.click();
+  };
+
+  return (
+    <section id="colorlib-hero" className="home-section">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-8 text-center">
+            <div className="intro">
+              <h1>Hi! I am Paing Su San</h1>
+              <h2>I am a Software Engineer</h2>
+              <p>
+                <button
+                  onClick={handleDownloadPDF}
+                  className="btn btn-primary btn-learn"
+                >
+                  Download Resume (PDF) <i className="icon-download4"></i>
+                </button>
+                &nbsp; {/* Add space between buttons */}
+                <button
+                  onClick={handleDownloadWord}
+                  className="btn btn-primary btn-learn"
+                >
+                  Download Resume (Word) <i className="icon-download4"></i>
+                </button>
+              </p>
             </div>
-          </li>
-          {/* <li style={{ backgroundImage: "url(images/ITGirl.jpg)" }}> */}
-          <li>
-            <div className="overlay"></div>
-            <div className="container-fluid">
-              <div className="row">
-                <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
-                  <div className="slider-text-inner">
-                    <div className="desc">
-                      <h1>
-                        I am <br />a Software Engineer
-                      </h1>
-                      <p>
-                        <a className="btn btn-primary btn-learn">
-                          View Portfolio <i className="icon-briefcase3"></i>
-                        </a>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </li>
-        </ul>
+          </div>
+        </div>
       </div>
     </section>
   );
